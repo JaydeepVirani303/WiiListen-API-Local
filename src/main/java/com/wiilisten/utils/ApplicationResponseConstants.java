6d9e6e5e9327c99e5fea7e636cd02cb97975ed4c
Dipak2611chauhan <dipakchauhan65420@gmail.com>
2025-08-08 20:02:47 +0530

    few changes apply in verify_otp api

--- src/main/java/com/wiilisten/utils/ApplicationResponseConstants.java
+++ src/main/java/com/wiilisten/utils/ApplicationResponseConstants.java
@@ -9,6 +9,7 @@ public final class ApplicationResponseConstants {
 	public static final String ACCESS_DENIED = "-1";
 	public static final String SUCCESS_RESPONSE_MESSAGE = "Success Response";
 	public static final String INVALID_REQUEST_MESSAGE = "Invalid Request";
+	public static final String INVALID_OTP = "Invalid OTP";
 
     private ApplicationResponseConstants() {
 
